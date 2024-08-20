b6d7de32c70c5e5233f6ef5b9f639b63a44f5a25
arindadl1 <arindalchar17@gmail.com>
2024-08-20 23:19:59 +0530

    added: assets, landing section

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,12 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Highlights = () => {
+  return (
+    <div>
+      Highlights
+    </div>
+  )
+}
+
+export default Highlights
